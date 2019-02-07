085da5d2e321d94d767acd2146148e63004f6c8a
nnFn <44121977+nnfn@users.noreply.github.com>
2019-02-07 09:56:58 +0900

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
 import socket
-import argparse
-
-def op():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--tcp",help="TCPでスキャンします",action="store_true")
-    args = parser.parse_args()
-    return args
 
 def tcp():
-    args = op()
-    if args.tcp:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcpip = input("InputIP>> ")
         tcpport = input("InputPort>> ")
@@ -21,8 +12,6 @@ def tcp():
         if code != 0:
             print(f"{tcpport}:Close")
 
-    
 
 if __name__ == '__main__':
-    op()
     tcp()
