46f46979295a2e1234d35aaa74a78d3554c57652
NyanyanFinalnyan <44121977+nnfn@users.noreply.github.com>
2019-03-23 04:39:50 +0900

    Update portscan.py
    いろいろ

--- portscan.py
+++ portscan.py
@@ -2,37 +2,30 @@
 import socket
 import argparse
 from scapy.all import *
+import threading
 
 def op():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tcp" ,help="TCPで直接繋いでスキャンします",action="store_true")
-    parser.add_argument("--syn", help="ハーフスキャンします",action="store_true")
-    parser.add_argument("--port", type=int, help="ポート番号を指定")
     parser.add_argument("--ip", help="IP&ホストを指定")
     args = parser.parse_args()
     return args
 
 args = op()
 
-if args.tcp:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    code = sock.connect_ex((args.ip,args.port))
-    sock.close()
-    if code == 0:
-        print(f"{args.port}/tcp Open")
-    if code != 0:
-        print(f"{args.port}/tcp Close")
-
-if args.syn:
+def syn(ports):
     sikens = random.randint(0, 1000)
-    syn=IP(dst=args.ip)/TCP(sport=RandShort(),dport=args.port,flags='S',seq=sikens)
-    syns=sr1(syn, timeout=5)
+    syn=IP(dst=args.ip)/TCP(sport=RandShort(),dport=ports,flags='S',seq=sikens)
+    syns=sr1(syn, timeout=5, verbose =0)
     if str(type(syns)) == "<class 'NoneType'>":
-        print(f"{args.port}/filtered")
+        pass
     elif syns[TCP].flags == "SA":
-        print(f"{args.port}/tcp Open")
+        print(f"{ports}/tcp Open")
     elif syns[TCP].flags == "RA":
-        print(f"{args.port}/tcp Closed")
+        print(f"{ports}/tcp Closed")
+
+for port in range(1, 9999):
+    thread = threading.Thread(target=syn, args=(port, ))
+    thread.start()
 
 if __name__ == '__main__':
     op()
