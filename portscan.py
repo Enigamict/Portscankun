40a8451861cdb3365c0c071ea2ce985c067fe54d
NyanyanFinalnyan <44121977+nnfn@users.noreply.github.com>
2019-03-23 15:54:48 +0900

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -6,26 +6,38 @@ import threading
 
 def op():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--tcp",help="TCPで直接繋いでスキャンします",action="store_true")
+    parser.add_argument("--syn",help="ハーフスキャンします",action="store_true")
     parser.add_argument("--ip", help="IP&ホストを指定")
     args = parser.parse_args()
     return args
 
 args = op()
 
+def tcp(ports1):
+    if args.tcp:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        code = sock.connect_ex((args.ip, ports1))
+        if code is 0:
+            print(f"{ports1}:OPEN")
+
 def syn(ports):
-    sikens = random.randint(0, 1000)
-    syn=IP(dst=args.ip)/TCP(sport=RandShort(),dport=ports,flags='S',seq=sikens)
-    syns=sr1(syn, timeout=5, verbose =0)
-    if str(type(syns)) == "<class 'NoneType'>":
-        pass
-    elif syns[TCP].flags == "SA":
-        print(f"{ports}/tcp Open")
-    elif syns[TCP].flags == "RA":
-        print(f"{ports}/tcp Closed")
+    if args.syn:
+        sikens = random.randint(0, 1000)
+        syn=IP(dst=args.ip)/TCP(sport=RandShort(),dport=ports,flags='S',seq=sikens)
+        syns=sr1(syn, timeout=5, verbose =0)
+        if str(type(syns)) == "<class 'NoneType'>":
+            pass
+        elif syns[TCP].flags == "SA":
+            print(f"{ports}/tcp Open")
+        elif syns[TCP].flags == "RA":
+            print(f"{ports}/tcp Closed")
 
 for port in range(1, 9999):
     thread = threading.Thread(target=syn, args=(port, ))
+    thread1 = threading.Thread(target=tcp, args=(port, ))
     thread.start()
+    thread1.start()
 
 if __name__ == '__main__':
     op()
